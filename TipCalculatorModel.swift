b6fe0db557201ad881d0321d8eb5024140db7834
Danny Chahla <dchahla@gmail.com>
2014-08-27 13:19:05 -0500

    first commit

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Daniel.Chahla on 8/27/14.
+//  Copyright (c) 2014 Daniel.Chahla. All rights reserved.
+//
+
+import Foundation
